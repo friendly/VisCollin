31a63fae73b78dca88da63db34d1076630ecb5e6
Michael Friendly <friendly@yorku.ca>
2023-08-17 19:07:10 -0400

    Add Acetylene example for centering

---
+++ examples/acetylene-colldiag.R
@@ -0,0 +1,30 @@
+#' ---
+#' title: Acetylene data
+#' ---
+
+library(VisCollin)
+library(car)
+library(dplyr)
+
+data(Acetylene, package = "genridge")
+
+# model with naive quadratic and interaction
+mod0 <- lm(yield ~ temp + ratio + time + I(time^2) + temp:time,
+           data=Acetylene)
+
+vif(mod0)
+
+print(colldiag(mod0, center=TRUE), fuzz = .3)
+
+# center the predictors
+Acetylene.centered <-
+  Acetylene |>
+  mutate(temp = temp - mean(temp),
+         time = time - mean(time))
+
+mod1 <- lm(yield ~ temp + ratio + time + I(time^2) + temp:time,
+           data=Acetylene.centered)
+
+vif(mod1)
+
+print(colldiag(mod1, center=TRUE), fuzz = .3)
