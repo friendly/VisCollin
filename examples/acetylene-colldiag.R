ca4a611148d5274482b2a277aced478b88c6cd2d
Michael Friendly <friendly@yorku.ca>
2023-08-19 21:49:24 -0400

    pkgdown it

--- examples/acetylene-colldiag.R
+++ examples/acetylene-colldiag.R
@@ -14,11 +14,8 @@ mod0 <- lm(yield ~ temp + ratio + time + I(time^2) + temp:time,
 
 vif(mod0)
 
-<<<<<<< HEAD
 print(colldiag(mod0, center=TRUE), fuzz = .3)
 
-=======
->>>>>>> 8a8ca654d635d6543f59eae8aa3e5794e67a34b2
 # center the predictors
 Acetylene.centered <-
   Acetylene |>
@@ -30,7 +27,4 @@ mod1 <- lm(yield ~ temp + ratio + time + I(time^2) + temp:time,
 
 vif(mod1)
 
-<<<<<<< HEAD
 print(colldiag(mod1, center=TRUE), fuzz = .3)
-=======
->>>>>>> 8a8ca654d635d6543f59eae8aa3e5794e67a34b2
